2c0d683aa38d87fc1190f868f1fea7ea8c3a019a
Balint86 <bankuti.bobo@gmail.com>
2026-01-13 21:52:35 +0100

    modified:   app/szolgaltatasok/megujulo/ipari/napelem-eromuvek/page.tsx modified:   app/szolgaltatasok/megujulo/ipari/page.tsx modified:   app/szolgaltatasok/megujulo/lakossagi/energiatarolo/page.tsx modified:   app/szolgaltatasok/megujulo/lakossagi/hoszivattyu/page.tsx modified:   app/szolgaltatasok/megujulo/lakossagi/napelem/page.tsx modified:   app/szolgaltatasok/megujulo/lakossagi/nyilaszaro-csere/page.tsx modified:   app/szolgaltatasok/megujulo/lakossagi/page.tsx modified:   app/szolgaltatasok/tavkozles/acelszerkezet-gyartas/page.tsx modified:   app/szolgaltatasok/tavkozles/acelszerkezet-gyartas/tornyok/elofeszitett-beton/page.tsx modified:   app/szolgaltatasok/tavkozles/acelszerkezet-gyartas/tornyok/lapostetos-tarto/page.tsx modified:   app/szolgaltatasok/tavkozles/acelszerkezet-gyartas/tornyok/monopole/page.tsx modified:   app/szolgaltatasok/tavkozles/acelszerkezet-gyartas/tornyok/page.tsx modified:   app/szolgaltatasok/tavkozles/acelszerkezet-gyartas/tornyok/racsos/page.tsx modified:   app/szolgaltatasok/tavkozles/acelszerkezet-gyartas/tornyok/satortetos-tarto/page.tsx modified:   app/szolgaltatasok/tavkozles/erosaramu-ellatas/0,4kv/page.tsx modified:   app/szolgaltatasok/tavkozles/erosaramu-ellatas/20kv/page.tsx modified:   app/szolgaltatasok/tavkozles/erosaramu-ellatas/page.tsx modified:   app/szolgaltatasok/tavkozles/mernoki-szolgaltatasok/acel-muszeres-vizsgalat/page.tsx modified:   app/szolgaltatasok/tavkozles/mernoki-szolgaltatasok/acel-statikai-vizsgalat/page.tsx modified:   app/szolgaltatasok/tavkozles/mernoki-szolgaltatasok/egyedi-acelszerkezet-tervezes/page.tsx modified:   app/szolgaltatasok/tavkozles/mernoki-szolgaltatasok/egyedi-acelszerkezet-tervezes/szalagkorlat-atalakitas/page.tsx modified:   app/szolgaltatasok/tavkozles/mernoki-szolgaltatasok/egyedi-acelszerkezet-tervezes/szeleromu-atalakitas/page.tsx modified:   app/szolgaltatasok/tavkozles/mernoki-szolgaltatasok/egyedi-acelszerkezet-tervezes/viztorony-atalakitas/page.tsx modified:   app/szolgaltatasok/tavkozles/mernoki-szolgaltatasok/felmeres/page.tsx modified:   app/szolgaltatasok/tavkozles/mernoki-szolgaltatasok/page.tsx modified:   app/szolgaltatasok/tavkozles/projekt-elokeszites/page.tsx modified:   app/szolgaltatasok/tavkozles/radio-kivitel/bazisallomasok/page.tsx modified:   app/szolgaltatasok/tavkozles/radio-kivitel/bsc-rnc-dwdm/page.tsx modified:   app/szolgaltatasok/tavkozles/radio-kivitel/mikro-osszekottetesek/page.tsx modified:   app/szolgaltatasok/tavkozles/radio-kivitel/page.tsx modified:   app/szolgaltatasok/tavkozles/tervezes/epiteszeti/page.tsx modified:   app/szolgaltatasok/tavkozles/tervezes/erosaramu-ellatasi/page.tsx modified:   app/szolgaltatasok/tavkozles/tervezes/page.tsx modified:   app/szolgaltatasok/tavkozles/tervezes/radio-elrendezesi/page.tsx modified:   app/szolgaltatasok/tavkozles/uzemeltetes/page.tsx

---
+++ app/szolgaltatasok/tavkozles/acelszerkezet-gyartas/tornyok/racsos/page.tsx
@@ -0,0 +1,9 @@
+export default function MegujuloPage() {
+    return (
+      <section style={{ padding: "32px 0" }}>
+        <h1>Megújuló</h1>
+        <p>Ide jön a megújuló leírása.</p>
+      </section>
+    );
+  }
+  
